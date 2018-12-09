9fce3bdea1da26817c761b6ebccfa58c47bdb9b3
John <john.a.cowie@gmail.com>
2018-12-09 21:50:51 +0000

    Move to 0.2.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject smidjen "0.2.0"
+(defproject smidjen "0.2.3-SNAPSHOT"
   :description "Just a smidjen of midje"
   :url "http://github.com/johncowie/smidjen"
   :license {:name "The MIT License"
